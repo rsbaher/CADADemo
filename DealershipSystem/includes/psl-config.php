9d8e014d5d003215b66eeff7dddcce74f49de514
rsbaher <reginabaher@gmail.com>
2016-04-08 08:27:47 -0600

    Update psl-config.php

--- DealershipSystem/includes/psl-config.php
+++ DealershipSystem/includes/psl-config.php
@@ -3,7 +3,7 @@
  * These are the database login details
  */  
 if (!defined('HOST')) define("HOST", "127.0.0.1");    // The host you want to connect to.
-if (!defined('USER')) define("USER", "sec_user");     // The database username. 
+if (!defined('USER')) define("USER", "sec_user1");     // The database username. 
 if (!defined('PASSWORD')) define("PASSWORD", "Password123!");         // The database password. 
 if (!defined('DATABASE')) define("DATABASE", "dealership_system");    // The database name.
  
